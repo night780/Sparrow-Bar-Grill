e977daaf3c2b7eec6d5ca68e905377fd8568be96
williamwittig2 <wwittig@student.greenriver.edu>
2022-06-06 17:58:33 -0400

    Made the data layer into a class

--- model/data-layer.php
+++ model/data-layer.php
@@ -1,18 +1,28 @@
 <?php
 
-// ---------- Get order options ----------
-// Get food
-function getFood()
+class DataLayer
 {
-    return array(12 => "Chicken Tenders", "Turkey Club", "Fresh Salmon",
-        "Mega Philly Cheese Melt", "1/3lb Cheeseburger",
-        "Crazy Spicy Skillet", "Sirloin Steak & Eggs", "Supreme Skillet",
-        "Smothered Cheese Fries", "Zesty Nachos", "Sweet Tangy Bacon Burger");
-}
+	private $_dbh;
 
-// Get drinks
-function getDrinks()
-{
-    return array(8 => "Bud Light", "Busch", "Sam Adams", "Corona",
-        "Mojito", "Cosmopolitan", "Negroni", "Whiskey Sour");
+	function __construct()
+	{
+		require_once($_SERVER['DOCUMENT_ROOT'].'/../config.php');
+		$dbh = 5;
+		$this->_dbh = $dbh;
+	}
+
+	// ---------- Get order options ----------
+	// Get food
+	static function getFood() {
+		return array("Chicken Tenders","Turkey Club","Fresh Salmon",
+			"Mega Philly Cheese Melt","1/3lb Cheeseburger",
+			"Crazy Spicy Skillet","Sirloin Steak & Eggs","Supreme Skillet",
+			"Smothered Cheese Fries","Zesty Nachos","Sweet Tangy Bacon Burger");
+	}
+
+	// Get drinks
+	static function getDrinks() {
+		return array("Bud Light","Busch","Sam Adams","Corona",
+			"Mojito","Cosmopolitan","Negroni","Whiskey Sour");
+	}
 }
