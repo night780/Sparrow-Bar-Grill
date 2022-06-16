e161c51ebc98d16403bd5785e4ebf83edbfe7e2c
williamwittig2 <wwittig@student.greenriver.edu>
2022-06-15 20:37:00 -0400

    Added functionality for member sign up and in

--- index.php
+++ index.php
@@ -4,14 +4,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//Session Starts here
-session_start();
-
 // Require the autoload file
 require_once('vendor/autoload.php');
-
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../config.php');
-
 
 // Create instance of the base class
 $f3 = Base::instance();
@@ -20,11 +15,15 @@ $f3 = Base::instance();
 $dataLayer = new DataLayer();
 $con = new Controller($f3);
 
+//Session Starts here
+session_start();
+
 /**
  * default page routing
  * @return void
  */
 $f3->route('GET /', function () {
+	var_dump($_SESSION);
     global $con;
     $con->home();
 });
@@ -34,6 +33,7 @@ $f3->route('GET /', function () {
  * @return void
  */
 $f3->route('GET /home', function () {
+	var_dump($_SESSION);
     global $con;
     $con->home();
 });
@@ -42,9 +42,29 @@ $f3->route('GET /home', function () {
  * VIP page routing
  * @return void
  */
-$f3->route('GET|POST /VIP', function () {
-    global $con;
-    $con->vip();
+$f3->route('GET|POST /VIP', function () use ($dbh) {
+	if (isset($_SESSION['member']) && get_class($_SESSION['member']) == "VIPMember") {
+		$sql="SELECT * FROM members";
+		$statement=$dbh->prepare($sql);
+		$statement->execute();
+		$result=$statement->fetchAll();
+
+		$memberNum=-1;
+		for ($i=0;$i<count($result);$i++) {
+			if ($_SESSION['member']->getEmail()==$result[$i]['email']) {
+				$memberNum=$result[$i]['memberNum'];
+				echo $memberNum;
+			}
+		}
+
+		var_dump($_SESSION);
+		// var_dump($result);
+		global $con;
+		$con->vip();
+	} else {
+		global $con;
+		$con->home();
+	}
 });
 
 /**
@@ -52,6 +72,7 @@ $f3->route('GET|POST /VIP', function () {
  * @return void
  */
 $f3->route('GET|POST /status', function () {
+	var_dump($_SESSION);
     global $con;
     $con->orderStatus();
 });
@@ -62,7 +83,7 @@ $f3->route('GET|POST /status', function () {
  * @return void
  */
 $f3->route('GET|POST /Order', function ($f3) {
-
+	var_dump($_SESSION);
     global $con;
     $con->order($f3);
 });
@@ -72,22 +93,19 @@ $f3->route('GET|POST /Order', function ($f3) {
  * @return void
  */
 $f3->route('GET|POST /confirmation', function () use ($dbh) {
-
+	var_dump($_SESSION);
     //1. define a query
-    $sql = "INSERT INTO orders (accountNum, food, drinks, total) VALUES (:accountNum, :food,:drinks,:total)";
+    $sql = "INSERT INTO orders (food, drinks, total) VALUES (:food,:drinks,:total)";
 
     //2. prepare a statement ($dbh is in config.php so cannot see in editor)
     $statement = $dbh->prepare($sql);
 
     //3. bind parameters
     //var_dump($_POST);
+    $food = $_SESSION['order']->getFood();
+    $drinks = $_SESSION['order']->getDrinks();
+    $total = $_SESSION['order']->getTotal();
 
-    $accountNum = 1234;
-    $food = $_SESSION['food'];
-    $drinks = $_SESSION['drinks'];
-    $total = $_SESSION['total'];
-
-    $statement->bindParam(':accountNum', $accountNum, PDO::PARAM_STR);
     $statement->bindParam(':food', $food, PDO::PARAM_STR);
     $statement->bindParam(':drinks', $drinks, PDO::PARAM_STR);
     $statement->bindParam(':total', $total, PDO::PARAM_STR);
@@ -104,6 +122,7 @@ $f3->route('GET|POST /confirmation', function () use ($dbh) {
  * @return void
  */
 $f3->route('GET|POST /contact', function () {
+	var_dump($_SESSION);
     global $con;
     $con->contact();
 });
@@ -113,22 +132,27 @@ $f3->route('GET|POST /contact', function () {
  * @return void
  */
 $f3->route('GET|POST /Sign-up', function () use ($dbh, $f3) {
+	// var_dump($_SESSION);
+	global $con;
+    $con->signUp($f3);
+});
+
+$f3->route('GET|POST /signUpConfirm', function () use ($dbh, $f3) {
+	// var_dump($_SESSION);
 	//1. define a query
 	$sql = "INSERT INTO members (fname, lname, email, pass, isVIP) 
-			VALUES (:fname, :lname,:email,:pass, :isVIP)";
+			VALUES (:fname, :lname, :email, :pass, :isVIP)";
 
 	//2. prepare a statement ($dbh is in config.php so cannot see in editor)
 	$statement = $dbh->prepare($sql);
 
 	//3. bind parameters
-	// var_dump($_SESSION);
-	$accountNum = 1234;
-	$fname = $_SESSION['fname'];
-	$lname = $_SESSION['lname'];
-	$email = $_SESSION['email'];
-	$pass = $_SESSION['pass'];
+	$fname = $_SESSION['member']->getFname();
+	$lname = $_SESSION['member']->getLname();
+	$email = $_SESSION['member']->getEmail();
+	$pass = $_SESSION['member']->getPassword();
 
-	if ($_SESSION['isVIP'] == "on") {
+	if (get_class($_SESSION['member']) == "VIPMember") {
 		$isVIP = "1";
 	} else {
 		$isVIP = "0";
@@ -140,30 +164,64 @@ $f3->route('GET|POST /Sign-up', function () use ($dbh, $f3) {
 	$statement->bindParam(':pass', $pass, PDO::PARAM_STR);
 	$statement->bindParam(':isVIP', $isVIP, PDO::PARAM_STR);
 
-	//4. execute
 	$statement->execute();
 
-    global $con;
-    $con->signUp($f3);
-});
+	$sql = "SELECT * FROM members";
+	$statement = $dbh->prepare($sql);
+	$statement->execute();
+	$result = $statement->fetchAll();
 
-/*
- * CREATE TABLE members (
-	memberNum int AUTO_INCREMENT,
-    fname VARCHAR(30) NOT NULL,
-    lname VARCHAR(30) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    pass VARCHAR(50) NOT NULL,
-    isVIP boolean,
-    PRIMARY KEY (memberNum)
-);
- */
+	for ($i = 0; $i < count($result); $i++) {
+		if ($_SESSION['member']->getEmail() == $result[$i]['email']) {
+			$memberNum = $result[$i]['memberNum'];
+			$_SESSION['member']->setMemberNum($memberNum);
+		}
+	}
+
+	global $con;
+	$con->signUpConfirm($f3);
+});
 
 /**
  * Sign-in form route
  * @return void
  */
-$f3->route('GET|POST /Sign-in', function ($f3) {
+$f3->route('GET|POST /Sign-in', function () use ($dbh, $f3) {
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$sql = "SELECT * FROM members";
+		$statement = $dbh->prepare($sql);
+		$statement->execute();
+		$result = $statement->fetchAll();
+		$found = false;
+
+		for ($i = 0; $i < count($result); $i++) {
+			if ($_POST['email'] == $result[$i]['email']) {
+				if ($result[$i]['isVIP'] == 1) {
+					$member = new VIPMember(0);
+					$member->setFname($result[$i]['fname']);
+					$member->setLname($result[$i]['lname']);
+					$member->setEmail($result[$i]['email']);
+					$member->setMemberNum($result[$i]['memberNum']);
+					$_SESSION['member'] = $member;
+				} else {
+					$member = new RestMember($result[$i]['fname'],
+						$result[$i]['lname'], $result[$i]['email'], $result[$i]['password']);
+					$member->setMemberNum($result[$i]['memberNum']);
+					$_SESSION['member'] = $member;
+				}
+				$found = true;
+			}
+		}
+
+		if ($found == false) {
+			$f3->set('errors["login"]', 'Please enter valid login information');
+		}
+		if ($found == true) {
+			header('location: home');
+		}
+	}
+
+	var_dump($_SESSION);
     global $con;
     $con->signIn($f3);
 });
@@ -173,8 +231,17 @@ $f3->route('GET|POST /Sign-in', function ($f3) {
  * @return void
  */
 $f3->route('GET|POST /login', function () {
+	var_dump($_SESSION);
     global $con;
     $con->logIn();
+});
+
+$f3->route('GET|POST /Sign-out', function () {
+	var_dump($_SESSION);
+	session_destroy();
+
+	global $con;
+	$con->home();
 });
 
 // Run fat free
